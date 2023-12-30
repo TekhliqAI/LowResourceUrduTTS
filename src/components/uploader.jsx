89c3d2e5c885d5b3e976d4bdb3ba407c2a9dbb38
RaoAkif <akifrao@gmail.com>
2023-12-30 15:23:46 +0500

    Switch from Dropzone to File Uploaded section

--- src/components/uploader.jsx
+++ src/components/uploader.jsx
@@ -1,7 +1,8 @@
 "use client";
-import { cn, bytesToMB, extractFileName } from "../lib/utils";
 import { useState } from "react";
 import Dropzone from "react-dropzone";
+import { cn, bytesToMB, extractFileName } from "../lib/utils";
+import Image from "next/image";
 
 const maxSize = 2097152;
 
@@ -14,64 +15,71 @@ const Uploader = () => {
     console.log(file);
     const data = new FormData();
     data.set("file", file);
+  };
 
-    // fetch("your_upload_url", {
-    //   method: "POST",
-    //   body: data,
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log("File uploaded successfully:", data);
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error uploading file:", error);
-    //   });
+  const handleReset = () => {
+    setSelectedFile(null);
   };
 
   return (
     <>
-      <Dropzone minSize={0} maxSize={maxSize} multiple={false} onDrop={onDrop}>
-        {({ getRootProps, getInputProps, isDragActive, isDragReject, fileRejections }) => (
-          <section className="text-black h-60 bg-[#F4F7FA] w-full p-8 rounded-2xl mt-8">
-            <div
-              {...getRootProps()}
-              className={cn(
-                " w-full h-full flex-col text-[#323232] text-xl  border-2 border-dashed border-black rounded-md flex items-center justify-center",
-                isDragActive ? "text-[#525252] border-[#525252] " : ""
-              )}
-            >
-              <input {...getInputProps()} />
-              {!isDragActive && "Click here or Drop image to upload"}
-              {isDragActive && !isDragReject && "Drop to Upload"}
-              {isDragReject && "File type is not acceptable"}
-              {/* {isFileTooLarge && <div className="mt-2">File is too large.</div>} */}
-            </div>
-          </section>
-        )}
-      </Dropzone>
+      {!selectedFile && (
+        <Dropzone
+          minSize={0}
+          maxSize={maxSize}
+          multiple={false}
+          onDrop={onDrop}
+        >
+          {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+            <section className="text-black h-60 bg-[#F4F7FA] w-full p-8 rounded-2xl mt-8">
+              <div
+                {...getRootProps()}
+                className={cn(
+                  " w-full h-full flex-col text-[#323232] text-xl  border-2 border-dashed border-black rounded-md flex items-center justify-center",
+                  isDragActive ? "text-[#525252] border-[#525252] " : ""
+                )}
+              >
+                <input {...getInputProps()} />
+                {!isDragActive && "Click here or Drop image to upload"}
+                {isDragActive && !isDragReject && "Drop to Upload"}
+                {isDragReject && "File type is not acceptable"}
+              </div>
+            </section>
+          )}
+        </Dropzone>
+      )}
 
       {selectedFile && (
-        <section className=" w-[800px] text-[#616161] py-4 bg-[#F4F7FA] px-8 rounded-md mt-8 ">
-          <div className="grid grid-cols-10 gap-1 text-[#000] border-b-black border-b-[1px] border-opacity-10  pb-2 mb-4">
-            <div className="col-span-8 ">Name</div>
-            <div className="col-span-1">Type</div>
-            <div className="col-span-1">Size</div>
+        <section className="w-[800px] text-[#616161] py-4 bg-[#F4F7FA] px-8 rounded-md mt-8 relative">
+          <div className="absolute top-2 right-3 cursor-pointer" onClick={handleReset}>
+            X
           </div>
-          <div className="grid grid-cols-10 gap-1">
-            <div className="col-span-8 ">
+          <div className="grid grid-cols-12 gap-1 text-[#000] border-b-black border-b-[1px] border-opacity-10 pb-2 mb-4">
+            <div className="col-span-2">Image</div>
+            <div className="col-span-6">Name</div>
+            <div className="col-span-2">Type</div>
+            <div className="col-span-2">Size</div>
+          </div>
+          <div className="grid grid-cols-12 gap-1 items-center">
+            <div className="col-span-2">
+              <Image
+                src={URL.createObjectURL(selectedFile)}
+                alt="Thumbnail"
+                width={20}
+                height={20}
+                className="h-12 w-12 object-cover"
+              />
+            </div>
+            <div className="col-span-6">
               <p>{extractFileName(selectedFile?.path)}</p>
             </div>
-            <div className="col-span-1">
+            <div className="col-span-2">
               <p>{selectedFile?.type.substring(6).toUpperCase()}</p>
             </div>
-            <div className="col-span-1">
+            <div className="col-span-2">
               <p>{bytesToMB(selectedFile?.size)}</p>
             </div>
           </div>
-
-          {/* <p>
-            {selectedFile?.path} - {bytesToMB(selectedFile?.size)}
-          </p> */}
         </section>
       )}
     </>
